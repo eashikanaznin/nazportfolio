76c42d2f1532cd069216551ba8389eadfc7b0d8e
eashikanaznin <eashika07@gmail.com>
2022-01-13 23:04:29 +0600

    custom module to expose api

---
+++ web/modules/custom/drunine_api/src/Controller/drunineController.php
@@ -0,0 +1,9 @@
+<?php
+namespace Drupal\drunine_api\Controller;
+class drunineController {
+  public function drunine() {
+    return array(
+      '#markup' => 'Welcome to our DRUNINE.'
+    );
+  }
+}
